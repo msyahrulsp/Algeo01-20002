942a7fce844195bd6d50a042b9d47d48cba7524b
ranjabi <fikriranjabi02@gmail.com>
2021-09-18 20:03:47 +0700

    add library in out/***.jar extension

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
-class HelloWorld {
+import static Library.Simple.add; // import from library located in .jar file
+
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(add(1,2)); // contoh call fungsi library nya
     }
 }
 
