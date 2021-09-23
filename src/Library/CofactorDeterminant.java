071e92a8d3b4f6766c38f08891be0d8620a33fc4
putriliza <16520063@std.stei.itb.ac.id>
2021-09-24 02:05:25 +0900

    add transpose cofactor adjoint

---
+++ src/Library/CofactorDeterminant.java
@@ -0,0 +1,46 @@
+package Library;
+
+public class CofactorDeterminant {
+    public static double getDeterminant(Matriks m) {
+        int N = m.baris;
+        double det;
+        if (N == 1) {
+            det = m.ELMT[0][0];
+        }
+        else if (N == 2) {
+            det = m.ELMT[0][0]*m.ELMT[1][1] - m.ELMT[1][0]*m.ELMT[0][1];
+        }
+        else {
+            det = 0;
+            for (int i=0; i<N; i++) {
+                det += Math.pow(-1, i) * m.ELMT[0][i] * getDeterminant(getMinor(m, 0, i));
+            }
+        }
+        return det;
+    }
+    public static Matriks getMinor(Matriks m, int x, int y) {
+        int N = m.baris;
+        Matriks mMinor = new Matriks(new double[N-1][N-1], N-1, N-1);
+        int minorBrs, minorKol;
+
+        minorBrs = 0;
+        for (int i = 0; i < N; i++) {
+            if (i == x) {
+                continue;
+            }
+            else {
+                minorKol = 0;
+                for (int j = 0; j < N; j++) {
+                    if (j == y) {
+                        continue;
+                    } else {
+                        mMinor.ELMT[minorBrs][minorKol] = m.ELMT[i][j];
+                        minorKol++;
+                    }
+                }
+                minorBrs++;
+            }
+        }
+        return mMinor;
+    }
+}
---
+++ src/Library/TransposeCofactorAdjoint.java
@@ -0,0 +1,30 @@
+package Library;
+
+public class TransposeCofactorAdjoint {
+    public static Matriks getTranspose(Matriks m) {
+        int N = m.baris;
+        Matriks mTranspose = new Matriks(new double[N][N], N, N);
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                mTranspose.ELMT[i][j] = m.ELMT[j][i];
+            }
+        }
+        return mTranspose;
+    }
+
+    public static Matriks getCofactor(Matriks m) {
+        int N = m.baris;
+        Matriks mCofactor = new Matriks(new double[N][N], N, N);
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                double detMinor = CofactorDeterminant.getDeterminant(CofactorDeterminant.getMinor(m, i, j));
+                mCofactor.ELMT[i][j] = Math.pow(-1, (i+j))*detMinor;
+            }
+        }
+        return mCofactor;
+    }
+
+    public static Matriks getAdjoint(Matriks m) {
+        return getTranspose(getCofactor(m));
+    }
+}
