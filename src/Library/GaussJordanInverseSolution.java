4275b84105018020578920fc23fb541c6d232801
ranjabi <fikriranjabi02@gmail.com>
2021-09-29 10:10:38 +0700

    fix jordan inverse

--- src/Library/GaussJordanInverseSolution.java
+++ src/Library/GaussJordanInverseSolution.java
@@ -4,13 +4,31 @@ import matriks.Matriks;
 
 public class GaussJordanInverseSolution {
     public static void jordanInverseSolution(Matriks m) {
-        GaussJordanInverse.makeInverse(m);
+        Matriks n = new Matriks(m.baris, m.kolom);
+        for (int i=0;i<n.baris;i++)
+        {
+            for (int j=0;j<n.kolom;j++)
+            {
+                n.ELMT[i][j] = m.ELMT[i][j];
+            }
+        }
         double det = GaussDeterminant.gaussDeterminant(m);
+        
         if (det == 0) {
             System.out.println("Matriks tidak memiliki inverse. (Determinan = 0)");
         } else {
+            n = GaussJordanInverse.makeInverse(n);
             System.out.println("Inverse Matriks: ");
-            m.displayMatriks();
+            // for (int i=0;i<n.baris;i++)
+            // {
+            //     for (int j=0;j<n.kolom;j++)
+            //     {
+            //         if ( 1/ n.ELMT[i][j] < 0 ) {
+            //             n.ELMT[i][j] = 0;
+            //         }
+            //     }
+            // }
+            n.displayMatriks();
         }
     }
     
